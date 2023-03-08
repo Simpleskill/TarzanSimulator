759cba62e0e95b301d23ea2fda9873984d2adf23
Simpleskill <andrefcastro1@hotmail.com>
2023-03-08 06:25:09 +0000

    First

---
+++ Assets/MyAssets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.MyAssets.Scripts
+{
+    public static class PlayerAnimations 
+    {
+        public static string IDLE = "Idle";
+        public static string RUN = "Run";
+        public static string JUMP = "Jump";
+        public static string FALLING = "Falling";
+        public static string IN_AIR = "InAir";
+        public static string DEATH = "Death";
+        public static string ROLL = "Roll";
+        public static string SWINGING = "Swinging"; 
+
+    }
+}
