fcf270ccc19b1b429a9eb3ec9265fb924f18b92d
Simpleskill <andrefcastro1@hotmail.com>
2023-03-08 16:21:38 +0000

    Walk, Jump And Rolling

--- Assets/_Game/_Scripts/Components/PlayerController.cs
+++ Assets/_Game/_Scripts/Components/PlayerController.cs
@@ -12,57 +12,119 @@ namespace SimplesDev.TarzanSimulator.Components
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
+        private Animator playerAnimator;
+        private Rigidbody playerRigidbody;
         [Header("Define the player ground check position")]
         [SerializeField] private Transform groundCheck;
 
         [Space]
-        
+
+        /**********         Movement         **********/
         [Header("Define the player basic movement speed")] 
         [SerializeField] private float movementSpeed;
 
+        /**********         Jumping         **********/
         [Space] [Header("Define the player jump force")] 
         [SerializeField] private float jumpForce;
-        
-        private Animator playerAnimator;
-        private Rigidbody playerRigidbody;
+        [SerializeField] private float jumpTime;
+        private float jumpTimeCounter;
+        private bool isJumping;
+        private bool isGoingToJump;
         private Ray groundCheckRay;
         private bool playerJumpInput;
+        private bool playerJumpInputUp;
+        private bool playerJumpInputDown;
+
+        /**********         Falling         **********/
+        private bool isFalling;
+        private bool isRolling;
+
 
         private void Start()
         {
             this.playerAnimator = this.GetComponent<Animator>();
             this.playerRigidbody = this.GetComponent<Rigidbody>();
+            InitializeVaraibles();
         }
 
         private void Update()
         {
             this.playerJumpInput = Input.GetKey(KeyCode.Space);
+            this.playerJumpInputUp = Input.GetKeyUp(KeyCode.Space);
+            this.playerJumpInputDown = Input.GetKeyDown(KeyCode.Space);
+            if (this.playerJumpInputDown && IsAnimatorReady()) isGoingToJump = true;
+            isFalling = (this.playerRigidbody.velocity.y < -0.1f) ? true : false;
+            if (this.playerRigidbody.velocity.y < -5) isRolling = true;
+            AnimatorHandler();
         }
         
         private void FixedUpdate()
         {
-            if (this.IsPlayerGrounded()) this.MovePlayer();
-            if (this.playerJumpInput && this.IsPlayerGrounded()) this.Jump();
+            this.MovePlayer();
+            if (this.isGoingToJump && this.IsPlayerGrounded()) this.Jump();
+            if (this.playerJumpInput && isJumping) this.JumpHigher();
+            if (this.playerJumpInputUp) this.StopJump();
+            if (isJumping && this.playerRigidbody.velocity.y < 0f && this.IsPlayerGrounded()) isJumping = false;
         }
 
         private bool IsPlayerGrounded()
         {
             this.groundCheckRay = new Ray(this.groundCheck.position, Vector3.down);
-            bool playerIsGrounded = Physics.SphereCast(this.groundCheckRay, 0.5f, LayerMask.GetMask("Ground"));
-            Debug.Log(playerIsGrounded);
-            return playerIsGrounded;
+            return Physics.Raycast(groundCheckRay, 0.7f, LayerMask.GetMask("Ground"));
         }
-        
       
         private void MovePlayer()
         {
-            this.playerRigidbody.MovePosition(this.playerRigidbody.position + Vector3.right * (this.movementSpeed * Time.fixedDeltaTime));
+            float airDecrease = (IsPlayerGrounded()) ? 1 : 0.8f;
+            this.playerRigidbody.MovePosition(this.playerRigidbody.position + Vector3.right * (this.movementSpeed * Time.fixedDeltaTime * airDecrease));
         }
 
         private void Jump()
         {
-            this.playerAnimator.SetBool("isJumping", true);
+            isGoingToJump = false;
             this.playerRigidbody.AddForce(Vector3.up * this.jumpForce, ForceMode.Impulse);
+            isJumping = true;
+            isRolling = false;
+            jumpTimeCounter = jumpTime;
+        }
+        private void JumpHigher()
+        {
+            if (jumpTimeCounter > 0)
+            {
+                this.playerRigidbody.AddForce(Vector3.up * (this.jumpForce *0.3f), ForceMode.Impulse);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        private void StopJump()
+        {
+            isJumping = false;
+        }
+
+        private void AnimatorHandler()
+        {
+            this.playerAnimator.SetBool("isGrounded", IsPlayerGrounded());
+            this.playerAnimator.SetBool("isJumping", isJumping);
+            this.playerAnimator.SetBool("isFalling", isFalling);
+            this.playerAnimator.SetBool("isRolling", isRolling);
+
+        }
+
+        private bool IsAnimatorReady()
+        {
+            return this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName(PlayerAnimations.RUN);
+        }
+
+        private void InitializeVaraibles()
+        {
+            isJumping = false;
+            isGoingToJump = false;
+            isFalling = false;
+            isRolling = false;
         }
     }
 }
